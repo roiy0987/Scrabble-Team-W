919c992a04331fb251829592608bb6432c9a67af
Arik Leikin <arik.leikin1998@gmail.com>
2023-06-04 13:32:10 +0300

    Added try and catch for IOException of socket connection + printed message for user to read that error occured. Changed design a little bit

--- src/main/java/View/View.java
+++ src/main/java/View/View.java
@@ -1,40 +1,67 @@
 package View;
 
+import Model.GuestModel;
+import Model.HostModel;
 import ViewModel.ScrabbleViewModel;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.DialogPane;
 import javafx.scene.control.TextField;
 import java.io.IOException;
 
 public class View {
 
     ScrabbleViewModel vm;
-
     @FXML
     TextField nickname;
-
     @FXML
     Button editButton;
+    @FXML
+    TextField ip;
+    @FXML
+    TextField port;
+    @FXML
+    DialogPane dialog;
+
 
     private TextField textField;
     public void joinGame() throws IOException {
         System.out.println("Join clicked!");
+        System.out.println(Integer.parseInt(port.getText()));
+        try{
+            GuestModel guest = new GuestModel(nickname.toString(), ip.getText(), Integer.parseInt(port.getText()));
+        }catch (IOException e){
+            e.printStackTrace();
+            dialog.setContentText("ERROR in connecting to server. Check ip and port! #!");
+        }
+
 //        vm = new ScrabbleViewModel(nickname,ip, port, false);
         // Perform your desired actions here
     }
 
     public void hostGame() throws IOException {
         System.out.println("Host clicked!");
+        try{
+            HostModel host = new HostModel(nickname.toString(),"localhost",Integer.parseInt(port.getText()));
+        }catch (IOException e){
+            e.printStackTrace();
+            dialog.setContentText("ERROR in connecting to server. Check ip and port! #2");
+        }
 //        vm = new ScrabbleViewModel(nickname,"localhost", 8889, true);
     }
 
-    public void editNickname() {
+    public void editParameters() {
         System.out.println("Edit nickname clicked! "+ nickname.getText());
+        dialog.setContentText("");
         if(editButton.getText().equals("EDIT")){
             nickname.setEditable(true);
+            ip.setEditable(true);
+            port.setEditable(true);
             editButton.setText("SAVE");
         }else {
             nickname.setEditable(false);
+            ip.setEditable(false);
+            port.setEditable(false);
             editButton.setText("EDIT");
         }
 
